bbea089bef1a7bf2ae41802ef4af1c53197ee473
stsaria <solothunder.autoer@gmail.com>
2024-02-15 09:32:04 +0900

    aaaa

--- cgi-bin/gchatrm.php
+++ cgi-bin/gchatrm.php
@@ -20,11 +20,11 @@
                 if (count($rows) >= 30){$rows = array_slice($rows, -30);}
             }
             foreach ($rows as $row){
-                $num_rm_thread++;
                 if ($row[0] == "../data/chat-".$_GET['thread'].".csv" && $row[3] == hash("fnv1a32", str_replace('.', '', $_SERVER['REMOTE_ADDR']))){
                     $filename = $row[0];
                     break;
                 }
+                $num_rm_thread++;
             }
             if($filename == "") {
                 if (isset($_SERVER['HTTP_REFERER'])){
@@ -43,7 +43,7 @@
     if ($fp){
         if (flock($fp, LOCK_EX)){
             unlink($filename);
-            $rows = array_splice($rows, $num_rm_thread);
+            unset($rows[$num_rm_thread]);
             if ($rows == []){fwrite($fp, null);}
             else{foreach ($rows as $row){fputcsv($fp, $row);}}
             flock($fp, LOCK_UN);
