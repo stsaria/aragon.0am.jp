2cbadc63f4cf5521acc3ddb6ae7ed1c49a93dd6b
stsaria <solothunder.autoer@gmail.com>
2024-03-09 18:43:25 +0900

    aaaaa

--- en/chat.php
+++ en/chat.php
@@ -15,7 +15,7 @@
         <link rel="icon" href="../img/favicon.ico">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../styles/style.css" rel="stylesheet" />
-        <title>Saadi/Chat</title>
+        <title>Aragon/Chat</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
     <body>
