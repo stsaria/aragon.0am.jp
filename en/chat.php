b8a3796cfac0a8706b61a7aa01d589880345d831
stsaria <solothunder.autoer@gmail.com>
2024-06-19 11:07:45 +0900

    Aragon ->Hispania

--- en/chat.php
+++ en/chat.php
@@ -15,7 +15,7 @@
         <link rel="icon" href="../img/favicon.ico">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../styles/style.css" rel="stylesheet" />
-        <title>Aragon/Chat</title>
+        <title>Hispania/Chat</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
